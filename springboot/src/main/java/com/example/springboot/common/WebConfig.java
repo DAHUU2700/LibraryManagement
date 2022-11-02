1040fa913115693676000dd74876753a78c659e1
DAHUU <dahuu2700@gmail.com>
2022-11-02 15:33:48 +0800

    09-JWT保护数据

---
+++ springboot/src/main/java/com/example/springboot/common/WebConfig.java
@@ -0,0 +1,28 @@
+package com.example.springboot.common;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements  WebMvcConfigurer {
+
+    @Autowired
+    JwtInterceptor jwtInterceptor;
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        // 指定controller统一的接口前缀（标识）
+        configurer.addPathPrefix("/api", clazz -> clazz.isAnnotationPresent(RestController.class));
+    }
+
+    // 加自定义拦截器JwtInterceptor，设置拦截规则
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtInterceptor).addPathPatterns("/api/**").excludePathPatterns("/api/admin/login");
+        //excludePathPatterns   放开登录页面，登录界面不拦截
+    }
+}
