362680986ff24b8b645bd6215af39e74c66fc628
DAHUU <dahuu2700@gmail.com>
2022-11-03 13:03:06 +0800

    10-滑块模组

--- vue/src/main.js
+++ vue/src/main.js
@@ -1,16 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 导入src下的css文件
+import '@/assets/global.css'
 
 //导入ElemetUI
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
-// 导入src下的css文件
-import '@/assets/global.css'
+//  导入滑块验证
+import SlideVerify from 'vue-monoplasty-slide-verify';
 
 Vue.config.productionTip = false
 Vue.use(ElementUI);
+Vue.use(SlideVerify);
 
 new Vue({
   router,
--- vue/src/views/login/Login.vue
+++ vue/src/views/login/Login.vue
@@ -1,5 +1,20 @@
 <template>
-  <div style="height: 100vh ; overflow: hidden">
+  <div style="height: 100vh ; overflow: hidden;position: relative">
+
+    <el-card class="cover" v-if="loginAdmin.id">
+      <slide-verify :l="42"
+                    :r="10"
+                    :w="310"
+                    :h="155"
+                    :imgs="imgs"
+                    :accuracy="6"
+                    slider-text="向右滑动"
+                    @success="onSuccess"
+                    @fail="onFail"
+                    @refresh="onRefresh"
+      ></slide-verify>
+    </el-card>>
+
     <div style="width: 500px; height: 400px;background-color: white;border-radius: 10px;margin: 150px auto;padding: 50px">
       <div style="margin: 30px;text-align: center;  font-size: 30px; font-weight: bold;color: dodgerblue">
         <span>登　录</span>
@@ -26,11 +41,17 @@
 
 import request from "@/utils/request";
 import Cookies from 'js-cookie';
+import img1 from '@/assets/slide/img1.jpg';
+import img2 from '@/assets/slide/img2.jpg';
+import img3 from '@/assets/slide/img3.jpg';
 
 export default {
   name: "LOGIN",
   data() {
     return {
+      //  滑块相关
+      loginAdmin: {},
+      imgs: [img1, img2, img3,],
       admin: {},
       rules: {
         username: [
@@ -50,24 +71,42 @@ export default {
             if (valid) {
               request.post('/admin/login', this.admin).then(res => {
                 if (res.code === '200') {
-                  this.$notify.success("登录成功")
-                  //  先存放Cookie数据
-                  if (res.data !== null) {
-                    Cookies.set('admin',JSON.stringify(res.data))
-                  }
-                  //  再跳转
-                  this.$router.push('/')
+                  //  滑块组件
+                  this.loginAdmin = res.data
                 } else {
                   this.$notify.error(res.msg)
                 }
               })
             }
           })
+        },
+        //  滑块
+        onSuccess(){
+          //  先存放Cookie数据
+          Cookies.set('admin',JSON.stringify(this.loginAdmin))
+          this.$notify.success("登录成功")
+          //  再跳转
+          this.$router.push('/')
+        },
+        onFail(){
+          console.log('onFail')
+        },
+        onRefresh(){
+          console.log('refresh')
         }
-      }
+      },
     }
 </script>
 
 <style scoped>
+.cover {
+  width: fit-content;
+  background-color: white;
+  position: absolute;
+  top:50%;
+  left:50%;
+  transform: translate(-50%, -50%);
+  z-index: 1000;
+}
 
 </style>
